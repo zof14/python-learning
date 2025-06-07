30fda50dc408b070cf0c85816f63d3c263fa4101
Zofia S <zosiasadel11@gmail.com>
2025-06-07 23:18:45 +0200

    data exploration and cleaning

--- plots.py
+++ plots.py
@@ -15,3 +15,27 @@ df_btc_price = pd.read_csv('Daily Bitcoin Price.csv')
 
 df_unemployment = pd.read_csv('UE Benefits Search vs UE Rate 2004-19.csv')
 df_unemployment.head()
+
+# Data Exploration
+#Tesla
+
+print(f'Largest value for Tesla in Web Search: {df_tesla.TSLA_WEB_SEARCH.max()}')
+print(f'Smallest value for Tesla in Web Search: {df_tesla.TSLA_WEB_SEARCH.min()} ')
+
+
+"""### Unemployment Data"""
+
+print(f'Largest value for Unemployemnt Benefits {df_unemployment.UE_BENEFITS_WEB_SEARCH.max()}')
+
+"""### Bitcoin"""
+
+print(f'largest BTC News Search: ')
+
+#DATA CLEANING
+print(f'Missing values for Tesla?: {df_tesla.isna().values.any()}')
+print(f'Missing values for U/E?: {df_unemployment.isna().values.any()}')
+print(f'Missing values for BTC Search?: {df_btc_search.isna().values.any()} ')
+
+print(f'Missing values for BTC price?:{df_btc_price.isna().values.any()} ')
+
+print(f'Number of missing values: {df_btc_price.isna().values.sum()}')
